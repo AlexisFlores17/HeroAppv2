03b64488c34ca7bd1df63416d8f513910f16e5bc
AlexisFlores17 <alex.p.flores17@gmail.com>
2025-01-29 20:29:35 -0600

    style: :lipstick: more fig animation for login page
    More rectangles and animation for login page

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -13,24 +13,40 @@ export const LoginPage = () => {
   return (
     <div className="login flex items-center justify-center h-screen">
       <div
-        className="square backdrop-blur-lg "
+        className="square backdrop-blur-sm "
         style={{ "--i": 0 } as React.CSSProperties}
       ></div>
       <div
-        className="square backdrop-blur-lg"
+        className="square backdrop-blur-sm"
         style={{ "--i": 1 } as React.CSSProperties}
       ></div>
       <div
-        className="square backdrop-blur-lg"
+        className="square backdrop-blur-sm"
         style={{ "--i": 2 } as React.CSSProperties}
       ></div>
       <div
-        className="square backdrop-blur-lg"
+        className="square backdrop-blur-sm"
         style={{ "--i": 3 } as React.CSSProperties}
       ></div>
       <div
-        className="square backdrop-blur-lg"
+        className="square backdrop-blur-sm"
         style={{ "--i": 4 } as React.CSSProperties}
+      ></div>
+      <div
+        className="square backdrop-blur-sm"
+        style={{ "--i": 5 } as React.CSSProperties}
+      ></div>
+      <div
+        className="square backdrop-blur-sm"
+        style={{ "--i": 6 } as React.CSSProperties}
+      ></div>
+      <div
+        className="square backdrop-blur-sm"
+        style={{ "--i": 7 } as React.CSSProperties}
+      ></div>
+      <div
+        className="square backdrop-blur-sm"
+        style={{ "--i": 8 } as React.CSSProperties}
       ></div>
       <div className="login-cristal backdrop-blur-[5px] w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg p-4 md:p-6 lg:p-8">
         <form className="flex flex-col gap-4" onSubmit={onSubmit}>
